50190551659b9b5d64bffff714fd654c5c48574f
Kevin Cruz <kevfaridc@gmail.com>
2023-02-16 17:12:59 -0500

    chore: config project

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react';
+import { StyleFunctionProps, mode } from '@chakra-ui/theme-tools';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+  styles: {
+    global: (props: StyleFunctionProps) => ({
+      body: {
+        bg: mode('#fff', '#121212')(props),
+      },
+    }),
+  },
+});
+
+export default theme;
