caadd817f5d5cbe9e4e5cfd72c5017ee043e7ba3
Kevin Cruz <kevfaridc@gmail.com>
2023-02-19 17:49:32 -0500

    fix(seo): add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import '@/styles/globals.css';
 import theme from '@/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LangProvider>
+      <Head>
+        <title>OpenMC</title>
+      </Head>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
